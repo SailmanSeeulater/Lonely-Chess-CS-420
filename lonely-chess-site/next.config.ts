1067bd8746fed9eb377057feb5e2a61bce5ec942
SailmanSeeulater <latersailor@gmail.com>
2026-05-05 20:35:29 -0700

    Changing the config

--- lonely-chess-site/next.config.ts
+++ lonely-chess-site/next.config.ts
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/Lonely-Chess-CS-420',
-    assetPrefix: 'https://sailmanseeulater.github.io/Lonely-Chess-CS-420',
     images: { unoptimized: true },
     typescript: {
         ignoreBuildErrors: true,
