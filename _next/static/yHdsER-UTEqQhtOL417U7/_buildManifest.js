08cb8a30befc69796bc486a9c901a20e505abad7
SailmanSeeulater <latersailor@gmail.com>
2026-05-05 18:02:53 -0700

    Updates

--- _next/static/yHdsER-UTEqQhtOL417U7/_buildManifest.js
+++ _next/static/yHdsER-UTEqQhtOL417U7/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Lonely-Chess-CS-420//_next/:path+",
+        "destination": "/Lonely-Chess-CS-420/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
