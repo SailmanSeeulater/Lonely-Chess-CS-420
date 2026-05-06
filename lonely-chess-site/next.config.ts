9b999934431433b15de71d2ca2ef1ecdd529efee
SailmanSeeulater <latersailor@gmail.com>
2026-05-05 20:34:27 -0700

    remove basePath for Vercel

--- lonely-chess-site/next.config.ts
+++ lonely-chess-site/next.config.ts
@@ -2,7 +2,7 @@
 const nextConfig = {
     output: 'export',
     basePath: '/Lonely-Chess-CS-420',
-    assetPrefix: '/Lonely-Chess-CS-420/',
+    assetPrefix: 'https://sailmanseeulater.github.io/Lonely-Chess-CS-420',
     images: { unoptimized: true },
     typescript: {
         ignoreBuildErrors: true,
